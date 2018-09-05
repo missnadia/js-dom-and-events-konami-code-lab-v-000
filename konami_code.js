91cdbf6d3ce5fcf5206ae989cdc2fad573e60dfc
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 19:51:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,9 @@
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   let index = 0;
 
-  function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown', function(e)) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
@@ -18,7 +17,7 @@ function init() {
     } else {
       index = 0;
     }
-  }
+  });
 }
 
 init();
