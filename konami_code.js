134bcd580fd8726eead501df5cdaee16d3c5f4ef
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-05 19:48:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,10 +7,10 @@ function init() {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
-    if (key === alphabet[index]) {
+    if (key === code[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
 
         index = 0;
